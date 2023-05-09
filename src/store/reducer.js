a73f33ec9d5290c5e6ba6de9fda961ae78ff595a
Ksenia <naomi355@yandex.ru>
2023-05-09 15:50:11 +0400

    eng is default

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,47 +1,50 @@
-import { LANGUAGES } from "../utils/constants";
-import { ANIMATION_IN, ANIMATION_OUT, CHANGE_LANGUAGE, FIRST_VIEW } from "./actions";
-
+import { LANGUAGES } from '../utils/constants';
+import {
+  ANIMATION_IN,
+  ANIMATION_OUT,
+  CHANGE_LANGUAGE,
+  FIRST_VIEW,
+} from './actions';
 
 const languageState = {
-    language: LANGUAGES[0]
-}
+  language: LANGUAGES[1],
+};
 
 export const languageReducer = (state = languageState, { type, payload }) => {
-    switch (type) {
-        case CHANGE_LANGUAGE:
-            return {
-                language: payload,
-            };
-        default:
-            return state;
-    }
-}
-
+  switch (type) {
+    case CHANGE_LANGUAGE:
+      return {
+        language: payload,
+      };
+    default:
+      return state;
+  }
+};
 
 const animationState = {
-    sectionIn: '',
-    sectionOut: '',
-    indx: 0
-}
+  sectionIn: '',
+  sectionOut: '',
+  indx: 0,
+};
 
 export const animationReducer = (state = animationState, { type, payload }) => {
-    switch (type) {
-        case ANIMATION_IN:
-            return {
-                ...state,
-                sectionIn: payload,
-            };
-        case ANIMATION_OUT:
-            return {
-                ...state,
-                sectionOut: payload,
-            };
-        case FIRST_VIEW:
-            return {
-                ...state,
-                indx: payload,
-            };
-        default:
-            return state;
-    }
-}
+  switch (type) {
+    case ANIMATION_IN:
+      return {
+        ...state,
+        sectionIn: payload,
+      };
+    case ANIMATION_OUT:
+      return {
+        ...state,
+        sectionOut: payload,
+      };
+    case FIRST_VIEW:
+      return {
+        ...state,
+        indx: payload,
+      };
+    default:
+      return state;
+  }
+};
